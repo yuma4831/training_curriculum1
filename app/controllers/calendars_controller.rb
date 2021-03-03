18ac67c5c41a95f925354d1b234214921b9faa4e
yuma <yuma.4831@gmail.com>
2021-03-03 18:09:27 +0900

    issue6の曜日表示修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,12 +36,12 @@ class CalendarsController < ApplicationController
       end
 
 
-      wday_num = @todays_date.wday
+      wday_num = @todays_date.wday + x
       if wday_num >= 7
-        wday_num = wday_num -7
+        wday_num = wday_num - 7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans:  today_plans, wday : wdays[(wday_num+x)] }
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans:  today_plans, wday: wdays[wday_num] }
 
       @week_days.push(days)
     end
